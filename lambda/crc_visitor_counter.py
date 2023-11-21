54e0010ff98da75eb2fc762a59295972b7ba3072
keithrobbo <139442519+keithrobbo@users.noreply.github.com>
2023-11-21 15:35:43 +0000

    Update crc_visitor_counter.py

--- lambda/crc_visitor_counter.py
+++ lambda/crc_visitor_counter.py
@@ -6,7 +6,7 @@ TableName = 'CRCVisitorCounterDB'
 def lambda_handler(event, context):
     
     '''
-    deata = client.get_item(
+    data = client.get_item(
         TableName='CRCVisitorCounterDB',
         Key = {
             'stat': {'S': 'view-count'}
